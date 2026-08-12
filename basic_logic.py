f9f5b8667e65827d30a9dffbe972ef253237c0e3
biplob-lab <biplobmojumder@gmail.com>
2026-08-13 01:57:12 +0600

    login code

--- basic_logic.py
+++ basic_logic.py
@@ -1,5 +1,15 @@
-age = 15
+"""age = 15
 if age<18:
     print("He is a adult")
 else:
-    print("He is a child")
+    print("He is a child")"""
+
+
+username = "Biplob Mojumder"
+password = "018300"
+
+if username == "Biplob Mojumder":
+    if password == "018300":
+        print("login sucessfull")
+else:
+    print("Unknown user")

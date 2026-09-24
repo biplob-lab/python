84a99355793abd8aeccb9c5c7fcee369dcedda89
biplob-lab <biplobmojumder@gmail.com>
2026-09-24 14:44:12 +0600

    Refactor function definitions and improve code organization

--- func.py
+++ func.py
@@ -1,5 +1,18 @@
-def my_function():
+""" def my_function():
   print("My name is function")
-my_function()
+my_function() """
 
 
+""" def add_numbers(num1,num2):#num1,num2 are peremetars
+    sum = num1 + num2
+    print(f"Sum: {sum}")
+
+add_numbers(20,30)#20,30 are arguments
+add_numbers(59,5000025) """
+
+
+def multify(a,b):
+    return(a*b)
+
+result = multify(20,6)
+print(f"Multification result is {result}")

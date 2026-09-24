213537253d38e6c27de4377926bea1f035b0393d
biplob-lab <biplobmojumder@gmail.com>
2026-09-25 01:25:33 +0600

    Fix formatting and add docstrings for multify and division functions

--- func.py
+++ func.py
@@ -11,8 +11,17 @@ add_numbers(20,30)#20,30 are arguments
 add_numbers(59,5000025) """
 
 
-def multify(a,b):
+""" def multify(a,b):
     return(a*b)
 
 result = multify(20,6)
-print(f"Multification result is {result}")
+print(f"Multification result is {result}") """
+
+
+""" def division(a,b):
+    c = a/b
+    return c
+devis = division(2,5)
+final = devis + 2
+print(final) """
+
